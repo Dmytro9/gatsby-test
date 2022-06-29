a37164974104670614d37ff8cf592c804c71724e
Dmytro Poliuliuk <dmytro.poliuliuk@simplybusiness.co.uk>
2022-06-29 17:55:07 +0300

    styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,19 @@
+import { Link } from "gatsby"
 import React from "react"
 import Layout from "../components/Layout"
+import * as styles from "../styles/home.module.css"
 
 export default function Home() {
   return (
     <Layout>
-      <section>
+      <section className={styles.header}>
         <div>
           <h2>Design</h2>
           <h3>Develop & Deploy</h3>
           <p>Something</p>
+          <Link className={styles.btn} to="projects">
+            Projects
+          </Link>
         </div>
       </section>
     </Layout>
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,11 +1,13 @@
 import React from "react"
 import Layout from "../../components/Layout"
+import * as styles from "../../styles/projects.module.css"
 
 export default function Projects() {
   return (
     <Layout>
-      <div>
-        <h1>Projects page</h1>
+      <div className={styles.portfolio}>
+        <h2>Portfolio</h2>
+        <h3>Projects & Websites I've Created</h3>
       </div>
     </Layout>
   )
